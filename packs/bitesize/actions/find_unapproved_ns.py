5a6e632ce43a27ba286de12f93d323e4405de223
Andy Moore <andy@impulsed.net>
2016-08-23 14:30:10 +0100

    first version of scripts

---
+++ packs/bitesize/actions/find_unapproved_ns.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import importlib
+import logging
+import os
+import json
+from datetime import datetime
+
+from pprint import pprint
+
+from st2actions.runners.pythonrunner import Action
+
+class GetUnapproved(Action):
+
+    def run(self, allns):
+
+        output = []
+
+        for ns in allns['items']:
+            if 'metadata' in ns and 'labels' in ns['metadata'] and ns['metadata']['labels'] is not None and 'status' in ns['metadata']['labels']:
+                output.append(ns['metadata']['name'])
+
+        if len(output) > 0:
+            print json.dumps(output)
+        else:
+            print "no unapproved"
+
+    def json_serial(self, obj):
+        """JSON serializer for objects not serializable by default json code"""
+
+        if isinstance(obj, datetime):
+            serial = obj.isoformat()
+            return serial
+        raise TypeError("Type not serializable")
+
