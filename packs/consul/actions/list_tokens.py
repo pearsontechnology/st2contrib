e18b853c721282d328c189ce288fa0efc2c6abed
Andy Moore <andy@impulsed.net>
2016-09-19 19:42:01 +0100

    return json in list_tokens action

--- packs/consul/actions/list_tokens.py
+++ packs/consul/actions/list_tokens.py
@@ -1,7 +1,9 @@
 from lib import action
 
+import json
+
 class ConsulAclListAction(action.ConsulBaseAction):
     def run(self):
 
         acl_list = self.consul.acl.list()
-        return acl_list
+        print json.dumps(acl_list)
