f28aa1a259d6fde1a70e73c8bc8c90174039cbfa
Andy Moore <andy@impulsed.net>
2016-09-19 19:47:25 +0100

    updated variable names

--- packs/bitesize/actions/create_ns_consul_rule.py
+++ packs/bitesize/actions/create_ns_consul_rule.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
-
 # given a namesapce and policy, create a consul rule 
 
 import json
-
 from st2actions.runners.pythonrunner import Action
 
 class CreateRule(Action):
-
-  def run(self, cluster, policy):
-
-    rule = { "key": { cluster: { "policy" : policy }}, "service": { cluster: { "policy" : policy }}, "event" : { cluster: { "policy" : policy } }}
-
+  def run(self, namespace, policy):
+    rule = { "key": { namespace: { "policy" : policy }}, "service": { namespace: { "policy" : policy }}, "event" : { namespace: { "policy" : policy } }}
     print json.dumps(rule)
