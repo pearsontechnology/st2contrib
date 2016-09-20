95cb20ceb0f41725a0ba484909e9bf06ea1aefe9
Andy Moore <andy@impulsed.net>
2016-09-20 10:30:48 +0100

    updated workflow to take advantage of mistral conditionals

--- packs/bitesize/actions/check_consul_acl.py
+++ packs/bitesize/actions/check_consul_acl.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-
 import json
-import sys
 
 from st2actions.runners.pythonrunner import Action
 
@@ -11,9 +9,9 @@ class CreateRule(Action):
 
     tokens = json.loads(tokens)
     for token in tokens:
-      #print json.dumps(token, sort_keys=True, indent=2)
-      #print "token['Name']: %s" % token['Name']
-      #print "cluster: %s" % cluster
       if token['Name'] == cluster:
-        print token['ID']
-        sys.exit(1)
+        print token['ID'],
+        return "match"
+
+    print ""
+    return "missed"
