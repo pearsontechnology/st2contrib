412b20f45d5badb7e4261aadde6cdf6d33e6ee36
Peter Idah <peter.idah@pearson.com>
2017-05-04 13:22:36 +0100

    disable ssl check

--- packs/bitesize/actions/create_keycloak_realm.py
+++ packs/bitesize/actions/create_keycloak_realm.py
@@ -9,7 +9,7 @@ class CreateKeycloakRealm(Action):
 
         environment = self.config.get('environment')
         domain = self.config.get('domain')
-        base_url = "http://auth" + "." + environment + "." + domain + "/"
+        base_url = "https://auth" + "." + environment + "." + domain + "/"
         token_endpoint_url = base_url + "auth/realms/master/protocol/openid-connect/token"
         realms_endpoint_url = base_url + "auth/admin/realms"
         keycloak_password = self.config.get('keycloak_password')
@@ -17,6 +17,7 @@ class CreateKeycloakRealm(Action):
         response = requests.post(
             token_endpoint_url,
             timeout=5,
+            verify=False,
             data={
                 "username": "admin",
                 "password": keycloak_password,
@@ -38,6 +39,8 @@ class CreateKeycloakRealm(Action):
         client = requests.post(
             realms_endpoint_url,
             headers=headers,
+            timeout=5,
+            verify=False,
             data=json.dumps(realm_data))
 
         if client.status_code not in [201, 409]:
