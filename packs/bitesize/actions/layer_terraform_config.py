d069d7255eae7b780bacc8e30bba498aa1bc970d
Andy Moore <andy@impulsed.net>
2017-05-18 15:12:00 +0100

    changes to allow tfvars to be added into pr-environment.sh

--- packs/bitesize/actions/layer_terraform_config.py
+++ packs/bitesize/actions/layer_terraform_config.py
@@ -16,28 +16,37 @@ class LayerTerraformConfig(Action):
 
     return data
 
-  def run(self, gdefaults=None, rdefaults=None, rspecifics=None):
+  def run(self, gdefaults=None, rdefaults=None, rspecifics=None, overrides=None):
 
     output = {}
     rspdata = {}
     gddata = {}
     rddata = {}
 
-    if rspecifics['data'] is not None:
-      rspdata = self._getkv(rspecifics['data'])
+    if rspecifics is not None:
+      if rspecifics['data'] is not None:
+        rspdata = self._getkv(rspecifics['data'])
 
-    if rdefaults['data'] is not None:
-      rddata = self._getkv(rdefaults['data'])
+    if rdefaults is not None:
+      if rdefaults['data'] is not None:
+        rddata = self._getkv(rdefaults['data'])
 
-    if gdefaults['data'] is not None:
-      gddata = self._getkv(gdefaults['data'])
+    if gdefaults is not None:
+      if gdefaults['data'] is not None:
+        gddata = self._getkv(gdefaults['data'])
+
+    if overrides is not None:
+      if overrides['data'] is not None:
+        ovdata = overrides['data']
 
     print json.dumps(gddata, sort_keys=True, indent=2)
     print json.dumps(rddata, sort_keys=True, indent=2)
     print json.dumps(rspdata, sort_keys=True, indent=2)
+    print json.dumps(ovdata, sort_keys=True, indent=2)
 
     output = gddata.copy()
     output.update(rddata)
     output.update(rspdata)
+    output.update(ovdata)
 
     return (True, output)
